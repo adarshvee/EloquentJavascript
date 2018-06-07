589e60bbe971ade4a259910416772ed4e1401c63
Adarsh Vijayaraghavan <adarshvijayaraghavan@gmail.com>
2018-06-06 23:14:23 -0400

    Intro, Chapter 1 - Basic datatypes and operators

---
+++ Chapter1/WierdTypeConversions.js
@@ -0,0 +1,25 @@
+console.log(8 * null)
+//0
+console.log("5" - 1)
+// 4
+console.log("5" + 1)
+//51
+console.log("five" * 2)
+// NaN
+console.log(false == 0)
+//true
+
+console.log(null == undefined)
+//true
+
+console.log(null == 0)
+//false
+
+console.log("" == false)
+//true
+console.log("" === false)
+//false
+console.log(null || "user")
+//user
+console.log("Agnes" || "User")
+//Agnes
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+This project is where I try to learn JavaScript in the old-school, formal way - using a book.
+I am using the free (and awesome) online book Eloquent JavaScript : https://eloquentjavascript.net/
+Will update this file with more elaborate content once I finish
